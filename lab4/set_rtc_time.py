06f6380fc32e0c9ba902403084f3253fdd78cfd4
James A Scott III <firehawk4427@gmail.com>
2026-02-18 21:33:31 -0800

    tryed commenting more

--- lab4/set_rtc_time.py
+++ lab4/set_rtc_time.py
@@ -1,3 +1,21 @@
+"""
+-------------------------------------------------------------------------------
+Project: Script to Set RTC Time on RV8803
+
+File:    set_rtc_time.py
+
+Purpose: Sets the real-time clock (RTC) on an RV8803 chip using system time.
+         Synchronizes the system clock with the RTC.
+
+Logic: 1. Initializes the RV8803 RTC module.
+       2. Sets the RTC time to the current system time.
+
+-------------------------------------------------------------------------------
+Author:  James Scott
+Date:    February 2026
+-------------------------------------------------------------------------------
+"""
+
 from helpers.rv8803 import RV8803
 
 def main():
