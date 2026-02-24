49320a2f636c11a8bdb9506811ef78fdc7fb5928
James A Scott III <firehawk4427@gmail.com>
2026-02-23 23:45:47 -0800

    camer aw set to 5

--- lab4/helpers/camera.py
+++ lab4/helpers/camera.py
@@ -66,11 +66,13 @@ class HQCameraRecorder:
             # Force the frame rate in the configuration controls
             config = self.picam2.create_video_configuration(
                 main={"size": (640, 480)},
-                controls={"FrameRate": fps} 
+                controls={"FrameRate": fps,
+                          "AwbMode": 5}        
             )
             print(f"Camera configured for 480p Video at {fps} FPS.")
         else:
-            config = self.picam2.create_still_configuration(main={"size": (640, 480)})
+            config = self.picam2.create_still_configuration(main={"size": (640, 480)},
+                                                            controls={"AwbMode": 5})
             print("Camera configured for High-Res Still.")
             
         self.picam2.configure(config)
